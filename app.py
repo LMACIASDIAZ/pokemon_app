44d30d417549a3dbbe8195fac725233b031f1380
LMACIASDIAZ <135484312+LMACIASDIAZ@users.noreply.github.com>
2023-12-13 20:04:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template, request
+from flask import Flask,render_template 
+import requests
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
@@ -13,6 +14,11 @@ db = SQLAlchemy(app)
 class pokemon(db.Model):
     id: Mapped[int] = mapped_column(db.Integer, primary_key=True,autoincrement=True)
     name: Mapped[str] = mapped_column(db.String, nullable=False)
+    height: Mapped[float] = mapped_column(db.Float, nullable=False)
+    weight: Mapped[float] = mapped_column(db.Float, nullable=False)
+    order: Mapped[int] = mapped_column(db.Integer, nullable=False)
+    type:  Mapped[str] = mapped_column(db.String, nullable=False)
+
 
 with app.app_context():
      db.create_all()
@@ -20,13 +26,23 @@ with app.app_context():
 
 def get_pokemon_data(pokemon):
     url= f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
-    r= request.get (url).json()
+    r= requests.get(url).json()
     return r
 
 
 @app.route("/")
 def home():
-    return render_template('pokemon.html')
+    data = get_pokemon_data('lucario')
+    pokemon= {
+            'id':data.get('name'),
+              'name':data.get('name'),
+              'height':data.get('height'),
+              'weight':data.get('weight'), 
+              'order': data.get('weight'),
+              'type': 'agua',
+              'photo': data.get('sprites').get('other').get('official-artwork').get('front_default')
+                    }
+    return render_template('pokemon.html', pokemon=pokemon)
 
 
 
@@ -34,7 +50,7 @@ def home():
 def new_pokemon():
     new_pokemon = 'pikachu'
     if new_pokemon:
-          obj = pokemon(name= new_pokemon) 
+          obj = pokemon(name= new_pokemon, height=1.50,weight= 60, order=100,type= 'electrico' ) 
           db.session.add(obj)
           db.session.commit()
     return 'Pokemon Agregado'
@@ -45,12 +61,25 @@ def select():
     Lista_pokemon = pokemon.query.all()
     for p in Lista_pokemon:
         print(p.name)
-    return 'pokemon'
+    return 'alo'
 
 @app.route('/select/<name>')
-def selectbyid(name):
+def selectbyname(name):
     poke= pokemon.query.filter_by(name=name).first()
-    return str(poke.id)
+    return str(poke.id) + str(poke.name)
+
+@app.route('/selectbyid/<id>')
+def selectbyid(id):
+    poke= pokemon.query.filter_by(id=id).first()
+    return str(poke.id) + str(poke.name)
+
+
+@app.route('/deletebyid/<id>')
+def deletebyid(id):
+    poke_eliminado= pokemon.query.filter_by(id=id).first()
+    db.session.delete(poke_eliminado)
+    db.session.commit()
+    return 'Pokemon Eliminado'
 
 
 
